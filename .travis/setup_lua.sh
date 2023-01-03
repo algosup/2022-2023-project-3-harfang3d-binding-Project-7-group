2a81a2c6d560c6845143a6cbee70c5be6166549b
MaximePagesAlgoSup <91247535+MaximePagesAlgoSup@users.noreply.github.com>
2023-01-03 16:16:16 +0100

    Update setup_lua.sh

--- .travis/setup_lua.sh
+++ .travis/setup_lua.sh
@@ -1,9 +1,9 @@
-curl http://www.lua.org/ftp/lua-5.3.2.tar.gz | tar xz
-pushd lua-5.3.2
+sudo curl http://www.lua.org/ftp/lua-5.3.2.tar.gz | tar xz
+sudo pushd lua-5.3.2
 
-LUA_HOME_DIR=$TRAVIS_BUILD_DIR/install/lua
+sudo LUA_HOME_DIR=$TRAVIS_BUILD_DIR/install/lua
 
-make linux -j
-make INSTALL_TOP="$LUA_HOME_DIR" install
+sudo make linux -j
+sudo make INSTALL_TOP="$LUA_HOME_DIR" install
 
-popd
+sudo popd
