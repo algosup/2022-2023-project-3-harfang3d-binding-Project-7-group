cb748b9c1fa814f0371883ad8badf836468d5038
MaximePagesAlgoSup <91247535+MaximePagesAlgoSup@users.noreply.github.com>
2023-01-06 10:27:47 +0100

    First Test

--- tests/basic_type_exchange.py
+++ tests/basic_type_exchange.py
@@ -96,3 +96,22 @@ func Test(t *testing.T) {
 	assert.Equal(t, AddIntByReference(&a, &b), 7, "should be the same.")
 }
 '''
+
+
+test_rust = '''\
+extern crate my_test;
+[%test]
+fn test() {
+	assert_eq!(my_test::return_int(), 8);
+	assert_eq!(my_test::return_float(), 8.0);
+	assert_eq!(my_test::return_const_char_ptr(), "const char * -> string");
+
+	assert_eq!(my_test::return_int_by_pointer(), 9);
+	assert_eq!(my_test::return_int_by_reference(), 9);
+
+	assert_eq!(my_test::add_int_by_value(3, 4), 7);
+	let a = 3;
+	let b = 4;
+	assert_eq!(my_test::add_int_by_pointer(&a, &b), 7);
+	assert_eq!(my_test::add_int_by_reference(a, b), 7);
+'''
