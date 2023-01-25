85a0207149c86b81f7154e2f0b6e81c564c0d877
Chaput-Mathieu <mathieu.chaput@algosup.com>
2023-01-25 11:11:46 +0100

    modify adder.py to add substract to add more functions

--- examples/simple_example/adder.py
+++ examples/simple_example/adder.py
@@ -1,10 +1,20 @@
 import lib
 
-def bind(gen):
-    gen.start('adder')
+def bind_adder(gen):
     gen.add_include('./adder.h', True)
-    lib.bind_defaults(gen)
     gen.bind_function('adder', 'int', ['int a', 'int b'])
     gen.bind_function('adder', 'float', ['float a', 'float b'])
+    
+
+def bind_subtracter(gen):
+    gen.add_include('./subtracter.h', True)
+    gen.bind_function('subtracter', 'int', ['int a', 'int b'])
+    gen.bind_function('subtracter', 'float', ['float a', 'float b'])
+
+def bind(gen):
+    gen.start('adder')
+    lib.bind_defaults(gen)
+    bind_adder(gen)
+    bind_subtracter(gen)
     gen.finalize()
     return gen.get_output()
