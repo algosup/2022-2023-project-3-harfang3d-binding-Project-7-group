235d4d6188b968066c0d415f06b54c877784ab62
MaximePagesAlgoSup <91247535+MaximePagesAlgoSup@users.noreply.github.com>
2023-02-10 15:02:32 +0100

    Harnessing the power of Lambda Calculus (:sunglasses_emoji:)

--- lang/rust.py
+++ lang/rust.py
@@ -1352,10 +1352,11 @@ uint32_t %s(void* p) {
 			else:
 				rust += " " + re.sub(r'(\[)(.*?)(\])', r'\1harfang.\2\3', doc) + "\n"
 
-			rust += "Option 1 func "
+			rust += "pub fn "
 			if not is_global:
-				rust += f"(pointer *{clean_name_with_title(classname)}) "
-			rust += f"{clean_name_with_title(name_rust)}"
+				rust += f"{clean_name_with_title(self._name)}{clean_name_with_title(classname)}{clean_name_with_title(name_rust)} "
+			else:
+				rust += f"{clean_name_with_title(self._name)}{clean_name_with_title(name_rust)}"
 
 			# add bounding_name to the overload function
 			if "bound_name" in proto["features"]:
@@ -1365,175 +1366,173 @@ uint32_t %s(void* p) {
 				rust += proto["suggested_suffix"]
 
 			# add input(s) declaration
-			rust += "("
+			rust += f"(_this : &mut {clean_name_with_title(classname)}"
+			has_previous_arg = True
 			if len(proto["args"]):
-				has_previous_arg = False
-				for argin in proto["argsin"]:
+				for argin in proto["args"]:
 					if has_previous_arg:
 						rust += " ,"
 
 					# check if the input is in feature constant group, overrite the type
 					if "features" in proto and "constants_group" in proto["features"] and str(argin["carg"].name) in proto["features"]["constants_group"]:
-						rust += f"{clean_name(argin['carg'].name)} {proto['features']['constants_group'][str(argin['carg'].name)]}"
+						rust += f"{clean_name(argin['carg'].name)} : {proto['features']['constants_group'][str(argin['carg'].name)]}"
 					else:
-						rust += f"{clean_name(argin['carg'].name)} {self.__get_arg_bound_name_to_rust(argin)}"
+						rust += f"{clean_name(argin['carg'].name)} : {self.__get_arg_bound_name_to_rust(argin)}"
 					has_previous_arg = True
 
 			rust += ")"
 
 			# add output(s) declaration
-			rust += "("
-			has_previous_ret_arg = False
 			if proto["rval"]["conv"]:
+				rust += " ->"
 				rust += self.__get_arg_bound_name_to_rust(proto["rval"])
-				has_previous_ret_arg = True
 			
-			# only add arg output, NOT ARG IN OUT (pass them by pointer, not return them)
-			if len(proto['args']):
-				for arg in proto['args']:
-					if 'arg_out' in proto['features'] and str(arg['carg'].name) in proto['features']['arg_out']:
-						if has_previous_ret_arg:
-							rust += " ,"
+			# # only add arg output, NOT ARG IN OUT (pass them by pointer, not return them)
+			# if len(proto['args']):
+			# 	for arg in proto['args']:
+			# 		if 'arg_out' in proto['features'] and str(arg['carg'].name) in proto['features']['arg_out']:
+			# 			if has_previous_ret_arg:
+			# 				rust += " ,"
 
-						rust += self.__get_arg_bound_name_to_rust(arg)
-						has_previous_ret_arg = True
-			rust += ")"
+			# 			rust += self.__get_arg_bound_name_to_rust(arg)
+			# 			has_previous_ret_arg = True
+			rust += ";\n"
 
-			# begin function declaration
-			rust += "{\n"
+			# # begin function declaration
+			# rust += "{\n"
 
-			# convert arg in to c
-			if len(proto["args"]):
-				for arg in proto["args"]:
-					# if arg out only, declare this value
-					if "arg_out" in proto["features"] and str(arg["carg"].name) in proto["features"]["arg_out"]:
-						arg_bound_name = self.__get_arg_bound_name_to_rust(arg)
+			# # convert arg in to c
+			# if len(proto["args"]):
+			# 	for arg in proto["args"]:
+			# 		# if arg out only, declare this value
+			# 		if "arg_out" in proto["features"] and str(arg["carg"].name) in proto["features"]["arg_out"]:
+			# 			arg_bound_name = self.__get_arg_bound_name_to_rust(arg)
 
-						if arg["carg"].ctype.is_pointer() or (hasattr(arg["carg"].ctype, "ref") and arg["carg"].ctype.ref == "&"):
-							# if it's a arg out and a class
-							if self.__get_is_type_class_or_pointer_with_class(arg["conv"]):
-								arg_bound_name = clean_name_with_title(f"new_{arg_bound_name.replace('*', '')}")
-								# find the constructor without arg
-								for arg_conv in self._bound_types:
-									if str(arg_conv.ctype) == str(arg["conv"].ctype) and hasattr(arg_conv, "constructor") and arg_conv.constructor is not None:
-										proto_args = self._build_protos(arg_conv.constructor["protos"])
-										break
-								else:
-									proto_args = None
+			# 			if arg["carg"].ctype.is_pointer() or (hasattr(arg["carg"].ctype, "ref") and arg["carg"].ctype.ref == "&"):
+			# 				# if it's a arg out and a class
+			# 				if self.__get_is_type_class_or_pointer_with_class(arg["conv"]):
+			# 					arg_bound_name = clean_name_with_title(f"new_{arg_bound_name.replace('*', '')}")
+			# 					# find the constructor without arg
+			# 					for arg_conv in self._bound_types:
+			# 						if str(arg_conv.ctype) == str(arg["conv"].ctype) and hasattr(arg_conv, "constructor") and arg_conv.constructor is not None:
+			# 							proto_args = self._build_protos(arg_conv.constructor["protos"])
+			# 							break
+			# 					else:
+			# 						proto_args = None
 								
-								id_proto_without_arg = ""
-								if proto_args is not None and len(proto_args) > 1:
-									for id_proto_arg, proto_arg in enumerate(proto_args):
-										if len(proto_arg['args']) <= 0:
-											# add bounding_name to the overload function
-											if "bound_name" in proto_arg["features"]:
-												id_proto_without_arg = proto_arg["features"]["bound_name"]
-											# if automatic suffix generated
-											elif "suggested_suffix" in proto_arg:
-												id_proto_without_arg = proto_arg["suggested_suffix"]
-											break
+			# 					id_proto_without_arg = ""
+			# 					if proto_args is not None and len(proto_args) > 1:
+			# 						for id_proto_arg, proto_arg in enumerate(proto_args):
+			# 							if len(proto_arg['args']) <= 0:
+			# 								# add bounding_name to the overload function
+			# 								if "bound_name" in proto_arg["features"]:
+			# 									id_proto_without_arg = proto_arg["features"]["bound_name"]
+			# 								# if automatic suffix generated
+			# 								elif "suggested_suffix" in proto_arg:
+			# 									id_proto_without_arg = proto_arg["suggested_suffix"]
+			# 								break
 
-								rust += f"{clean_name(arg['carg'].name)} := {arg_bound_name}{id_proto_without_arg}()\n"
-							else:
-								# not a class, remove the * and make a new
-								rust += f"{clean_name(arg['carg'].name)} := new({arg_bound_name.replace('*', '')})\n"
-						else:
-							rust += f"var {clean_name(arg['carg'].name)} {arg_bound_name}\n"
+			# 					rust += f"{clean_name(arg['carg'].name)} := {arg_bound_name}{id_proto_without_arg}()\n"
+			# 				else:
+			# 					# not a class, remove the * and make a new
+			# 					rust += f"{clean_name(arg['carg'].name)} := new({arg_bound_name.replace('*', '')})\n"
+			# 			else:
+			# 				rust += f"var {clean_name(arg['carg'].name)} {arg_bound_name}\n"
 
-					c_call = ""
-					if arg["conv"]:
-						c_call = self.__arg_from_rust_to_c(arg, arg['carg'].name)
-					if c_call != "":
-						rust += c_call
-					else:
-						rust += f"{clean_name(arg['carg'].name)}ToC := {clean_name(arg['carg'].name)}\n"
+			# 		c_call = ""
+			# 		if arg["conv"]:
+			# 			c_call = self.__arg_from_rust_to_c(arg, arg['carg'].name)
+			# 		if c_call != "":
+			# 			rust += c_call
+			# 		else:
+			# 			rust += f"{clean_name(arg['carg'].name)}ToC := {clean_name(arg['carg'].name)}\n"
 
-			# declare arg out
-			if retval != "":
-				rust += "retval := "
+			# # declare arg out
+			# if retval != "":
+			# 	rust += "retval := "
 
-			if is_constructor:
-				rust += f"C.{clean_name_with_title(self._name)}Constructor{clean_name_with_title(name)}"
-			else:
-				rust += f"C.{clean_name_with_title(self._name)}{clean_name_with_title(name)}"
+			# if is_constructor:
+			# 	rust += f"C.{clean_name_with_title(self._name)}Constructor{clean_name_with_title(name)}"
+			# else:
+			# 	rust += f"C.{clean_name_with_title(self._name)}{clean_name_with_title(name)}"
 
-			# is global, add the Name of the class to be sure to avoid double name function name
-			if not is_global:
-				rust += f"{clean_name_with_title(convClass.bound_name)}"
+			# # is global, add the Name of the class to be sure to avoid double name function name
+			# if not is_global:
+			# 	rust += f"{clean_name_with_title(convClass.bound_name)}"
 
-			# add bounding_name to the overload function
-			if "bound_name" in proto["features"]:
-				rust += proto["features"]["bound_name"]
-			# if automatic suffix generated
-			elif "suggested_suffix" in proto:
-				rust += proto["suggested_suffix"]
+			# # add bounding_name to the overload function
+			# if "bound_name" in proto["features"]:
+			# 	rust += proto["features"]["bound_name"]
+			# # if automatic suffix generated
+			# elif "suggested_suffix" in proto:
+			# 	rust += proto["suggested_suffix"]
 
-			rust += "("
-			if not is_global and not is_constructor:
-				rust += "pointer.h, "
+			# rust += "("
+			# if not is_global and not is_constructor:
+			# 	rust += "pointer.h, "
 
-			if len(proto["args"]):
-				has_previous_arg = False
-				for arg in proto["args"]:
-					if has_previous_arg:
-						rust += " ,"
+			# if len(proto["args"]):
+			# 	has_previous_arg = False
+			# 	for arg in proto["args"]:
+			# 		if has_previous_arg:
+			# 			rust += " ,"
 
-					# special Slice
-					if isinstance(arg["conv"], lib.rust.stl.RustSliceToStdVectorConverter):
-						slice_name = clean_name(arg['carg'].name)
-						if "RustConstCharPtrConverter" in str(arg["conv"].T_conv) or \
-							"RustStringConverter" in str(arg["conv"].T_conv):	
-							slice_name = f"{slice_name}SpecialString"
-						# if it's a class, get a list of pointer to c class
-						elif self.__get_is_type_class_or_pointer_with_class(arg["conv"].T_conv):
-							slice_name = f"{slice_name}Pointer"
-						rust += f"{slice_name}ToCSize, {slice_name}ToCBuf"
-					else:
-						# if (arg['carg'].ctype.is_pointer() or (hasattr(arg['carg'].ctype, 'ref') and arg['carg'].ctype.ref == "&")) and \
-						# 	arg['conv'].bound_name != "string" and not arg['conv'].is_type_class():
-						# 	rust += "&"
-						rust += f"{clean_name(arg['carg'].name)}ToC"
+			# 		# special Slice
+			# 		if isinstance(arg["conv"], lib.rust.stl.RustSliceToStdVectorConverter):
+			# 			slice_name = clean_name(arg['carg'].name)
+			# 			if "RustConstCharPtrConverter" in str(arg["conv"].T_conv) or \
+			# 				"RustStringConverter" in str(arg["conv"].T_conv):	
+			# 				slice_name = f"{slice_name}SpecialString"
+			# 			# if it's a class, get a list of pointer to c class
+			# 			elif self.__get_is_type_class_or_pointer_with_class(arg["conv"].T_conv):
+			# 				slice_name = f"{slice_name}Pointer"
+			# 			rust += f"{slice_name}ToCSize, {slice_name}ToCBuf"
+			# 		else:
+			# 			# if (arg['carg'].ctype.is_pointer() or (hasattr(arg['carg'].ctype, 'ref') and arg['carg'].ctype.ref == "&")) and \
+			# 			# 	arg['conv'].bound_name != "string" and not arg['conv'].is_type_class():
+			# 			# 	rust += "&"
+			# 			rust += f"{clean_name(arg['carg'].name)}ToC"
 
-					has_previous_arg = True
-			rust += ")\n"
-			ret_args = []
-			if retval != "":
-				src, retval_rust = self.__arg_from_c_to_rust(proto["rval"], "retval")
-				rust += src
+			# 		has_previous_arg = True
+			# rust += ")\n"
+			# ret_args = []
+			# if retval != "":
+			# 	src, retval_rust = self.__arg_from_c_to_rust(proto["rval"], "retval")
+			# 	rust += src
 
-				ret_args.append(retval_rust)
+			# 	ret_args.append(retval_rust)
 
-			# return arg out
-			# only add arg output, NOT ARG IN OUT (pass them by pointer, not return them)
-			if "arg_out" in proto["features"]:
-				for arg in proto['args']:
-					if 'arg_out' in proto['features'] and str(arg['carg'].name) in proto['features']['arg_out']:
-						# add name
-						retval_rust = clean_name(str(arg["carg"].name))
-						# if it's a arg out and a class, don't convert because it was already done upper
-						if not self.__get_is_type_class_or_pointer_with_class(arg["conv"]):
-							retval_rust = clean_name(str(arg["carg"].name)) + "ToC"
-							src, retval_rust = self.__arg_from_c_to_rust(arg, retval_rust)
-							rust += src
+			# # return arg out
+			# # only add arg output, NOT ARG IN OUT (pass them by pointer, not return them)
+			# if "arg_out" in proto["features"]:
+			# 	for arg in proto['args']:
+			# 		if 'arg_out' in proto['features'] and str(arg['carg'].name) in proto['features']['arg_out']:
+			# 			# add name
+			# 			retval_rust = clean_name(str(arg["carg"].name))
+			# 			# if it's a arg out and a class, don't convert because it was already done upper
+			# 			if not self.__get_is_type_class_or_pointer_with_class(arg["conv"]):
+			# 				retval_rust = clean_name(str(arg["carg"].name)) + "ToC"
+			# 				src, retval_rust = self.__arg_from_c_to_rust(arg, retval_rust)
+			# 				rust += src
 							
-						ret_args.append(retval_rust)
+			# 			ret_args.append(retval_rust)
 
-			if len(ret_args) > 0:
-				rust += "return "
-			has_previous_arg = False
-			for retarg in ret_args:
-				if has_previous_arg:
-					# check and remove "\n" just in case
-					if rust[-1] == "\n":
-						rust = rust[:-1]
-					rust += ", "
-				has_previous_arg = True
-				rust += retarg
+			# if len(ret_args) > 0:
+			# 	rust += "return "
+			# has_previous_arg = False
+			# for retarg in ret_args:
+			# 	if has_previous_arg:
+			# 		# check and remove "\n" just in case
+			# 		if rust[-1] == "\n":
+			# 			rust = rust[:-1]
+			# 		rust += ", "
+			# 	has_previous_arg = True
+			# 	rust += retarg
 				
-			# check and remove "\n" just in case
-			if rust[-1] == "\n":
-				rust = rust[:-1]
-			rust += "\n}\n"
+			# # check and remove "\n" just in case
+			# if rust[-1] == "\n":
+			# 	rust = rust[:-1]
+			# rust += "\n}\n"
 
 		return rust
 
